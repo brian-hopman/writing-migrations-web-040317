02826b3333158597f2a1a8cc2b21c552c2232e7f
brian-hopman <hopman_2@hotmail.com>
2017-04-13 14:40:38 -0400

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,15 @@
+
+
 class CreateStudents < ActiveRecord::Migration
+
+
+    #ActiveRecord::Migration
+
+    def change
+      create_table :students do |t|
+        t.string :name
+      end
+    end
+
+
 end
